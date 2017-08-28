1081710c84f9210d53b5ff9eeb6f5cd419d40633
凤朝凰 <373202015@qq.com>
2017-08-29 00:01:58 +0800

    add a test artical

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 #wangbinds.coding.me
 这个项目用于生成我的个人静态网站。
+
+# 开发说明
+本网站使用[Jekyll](jekyll.com)自动生成。目前在本地Ubuntu 12.10和Fedora 26中通过
+```
+$ sudo apt-get install rubygems
+$ sudo dnf install rubygems
+$ gem install jekyll
+```
+的方式安装Jekyll都失败了，原因不明。
---
+++ _posts/2017-08-28-first-artical.mk
@@ -0,0 +1,21 @@
+---
+layout: post
+title:  "我的第一篇文章"
+date:   2017-08-28 18:54:05 +0800
+categories: test
+---
+[toc]
+# 我的第一篇文章
+这篇文章是用来测试Jekyll的。
+
+## markdown语法
+* unordered list 1
+* unordered list 2
+
+1. ordered list 1
+1. ordered list 2
+
+### 表格
+|表头1|表头2|
+|-----|-----|
+|内容1|内容2|
